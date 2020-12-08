2d5c7a0554e492f3e3d5bf16b0be14b2ee7bf571
Kashyapi94 <75570029+Kashyapi94@users.noreply.github.com>
2020-12-08 22:33:10 +0530

    Start
    Star

---
+++ Star.java
@@ -0,0 +1,45 @@
+class Star
+
+{
+
+public static void main(String[] args)
+
+{
+
+ 
+
+for(int a=0;a<=5;a++)
+
+{
+
+for(int b=1;b<=a;b++)
+
+{
+
+System.out.print("* ");
+
+}  
+
+  System.out.println("*");
+
+}
+
+for(int c=5;c>=0;c--)
+
+{
+
+for(int d=1;d<=c;d++)
+
+{
+
+System.out.print("* ");
+
+}
+
+System.out.println("*");
+
+}
+
+} 
+
+} 
